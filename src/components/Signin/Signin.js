27e4bea9b48d25f06d296cef79d724e25c86001e
ShibbyBro <tanneracord@gmail.com>
2020-06-09 19:12:01 -0700

    updating things

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
   onSubmitSignIn = () => {
     fetch('http://localhost:3000/signin', {
       method: 'post',
-      headers: {'Content-Type': 'application/json'},
+      headers: {'Content-Type':'application/json'},
       body: JSON.stringify({
         email: this.state.signInEmail,
         password: this.state.signInPassword
